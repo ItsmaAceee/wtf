660f5faa4253b4d4a4820b1818d21968e5284936
Its Ma Ace <113987065+ItsmaAceee@users.noreply.github.com>
2023-09-07 03:24:09 +0000

    pre/post increment

---
+++ operator.js
@@ -0,0 +1,9 @@
+let x= 3
+let y= x++     /* paila y=x assign garne paxi x lai increment garne i.e post increment */
+console.log(`X = ${x}`)
+console.log(`y = ${y}`)
+console.log()
+let a= 3
+let b= ++a     /* paila increment a paxi store in b i.e pre increment */
+console.log(`a = ${a}`)
+console.log(`b = ${b}`)
